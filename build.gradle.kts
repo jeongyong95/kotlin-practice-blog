bb7896cc5ab50717236124903a97fc34d5bb632a
jeongyong <jeongyong95@gmail.com>
2023-02-25 09:43:29 +0900

    [KOTLIN-BLOG-2] replace postgres to mysql

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
